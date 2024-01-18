4e49b02c310454b00a9e65ddbe48d72420f834a3
nikeshsingh1947 <141706194+nikeshsingh1947@users.noreply.github.com>
2024-01-18 09:48:12 +0000

    Build: (b7c0309) cms

--- src/Redux/store.jsx
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore, combineReducers, applyMiddleware,compose } from "redux";
-import thunk from "redux-thunk";
-import  handleReducer  from "./reducer";
-import { AuthReducer } from "./AuthRedux/reducer";
-const rootReducer = combineReducers({ imageData: handleReducer,userData:AuthReducer });
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
